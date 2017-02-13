4c232835ebf59c2cab9c94a399711af5a633e6f0
Riccardo Deri <riccardoallegro@gmail.com>
2017-02-13 20:04:21 +0100

    Modifiche dummy per branch "windows"

--- tests/FakeTest.php
+++ tests/FakeTest.php
@@ -4,15 +4,24 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 use App\Baseball;
 
+/**
+ * Aggiungo un commento.
+ * Questa modifica dovrebbe essere visibile solo nel branch "windows"
+ */
 class FakeTest extends TestCase
 {
-    public function test()
+    public function testTrue()
     {
         $this->assertTrue(true);
     }
 
-    public function testName1()
+    public function testTrue2()
     {
         $this->assertTrue(true);
+    }
+
+    public function testFalse()
+    {
+        $this->assertFalse(true);
     }
 }
