27cd4d2c7d6fa7f2c90a7fa9707a01b9a3ac7722
riccardo <man.in.the.bolla@gmail.com>
2017-02-13 20:08:48 +0100

    Modifiche al branch "ubuntu"

--- tests/FakeTest.php
+++ tests/FakeTest.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 use PHPUnit\Framework\TestCase;
 use App\Baseball;
-
+/**
+* Commento inutile per il branch "ubuntu"
+*/
 class FakeTest extends TestCase
 {
     public function test()
